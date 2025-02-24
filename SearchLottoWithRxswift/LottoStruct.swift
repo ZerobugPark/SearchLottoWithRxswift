f5d18f81b93f14d7d3f44266620383ace49850b6
young <zerobugpark@gmail.com>
2025-02-24 18:59:17 +0900

    로또 API 구조체 추가

---
+++ SearchLottoWithRxswift/LottoStruct.swift
@@ -0,0 +1,20 @@
+//
+//  LottoStruct.swift
+//  SearchLottoWithRxswift
+//
+//  Created by youngkyun park on 2/24/25.
+//
+
+import Foundation
+
+struct Lottery: Decodable {
+    let drwNoDate: String
+    let drwtNo6: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let bnusNo: Int
+    let drwNo: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo1: Int
+}
